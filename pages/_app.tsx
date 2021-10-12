03b651561d79ba0e93e6a9f4ad812b7157a36d0e
Chris Kjær Sørensen <mail@chriskjaer.dk>
2021-10-12 22:40:31 +0200

    nextjs migration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import 'tailwindcss/tailwind.css'
+import '../base.global.css'
 
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { DefaultSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <div>
+        <DefaultSeo
+          title="chriskjaer"
+          description="Creative Developer, Software Engineer, Co-Founder of Gaest.com. ex-Airbnb. Now CTO & Co-Founder at Landfolk. I code stuff and make it pretty."
+        />
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,47 @@
-import Head from 'next/head'
+import Image from 'next/image'
+import { ComponentProps } from 'react'
+
+import logo from '@/public/chriskjaer-logo.svg'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex items-center justify-center px-4">
+        <span className="w-3 h-3 mr-0 ns:(mr-4 mt-0)">
+          <Image src={logo} alt="chriskjaer logo" />
+        </span>
+
+        <div className="pl-4 py-3 ml-4 border-l border-gray-300">
+          <h1 className="font-bold font-serif text-f2.5">
+            Chris Kjær Sørensen
+          </h1>
+
+          <p className="text-f6 ns:text-f5">
+            Creative Developer, Software Engineer. CTO & Co-Founder of{' '}
+            <A href="https://landfolk.com" rel="Landfolk.com">
+              Landfolk
+            </A>
+          </p>
+
+          <p className="italic text-f6 mt-1">
+            Previously: Co-Founder of{' '}
+            <A href="https://gaest.com" rel="Gaest.com">
+              Gaest.com
+            </A>{' '}
+            ex-Airbnb.
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-5 ns:mb-5 text-f7 space-x-3">
+        <A href="https://www.linkedin.com/in/chriskjaer/">LinkedIn</A>
+        <A href="https://github.com/chriskjaer">Github</A>
+      </div>
     </div>
   )
 }
+
+const A = ({ className, ...props }: ComponentProps<'a'>) => (
+  // eslint-disable-next-line jsx-a11y/anchor-has-content
+  <a {...props} className={['text-orange underline', className].join(' ')} />
+)
